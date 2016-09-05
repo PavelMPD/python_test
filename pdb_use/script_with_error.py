a025cab2cd454b8aa45626e089c7ebc6925b7d1d
Pavel Matveev <p.matveev@itransition.com>
2016-09-05 13:52:36 +0300

    Add pdb command description

--- pdb_use/script_with_error.py
+++ pdb_use/script_with_error.py
@@ -1,5 +1,9 @@
 import pdb
 # pdb.pm()
+# python -m pdb script.py
+# c - continue
+# s - step
+# q - quite
 
 ZERO = 0
 
@@ -9,5 +13,5 @@ def division(dividend, divisor):
 
 print(__name__)
 
-#if __name__ == "__main__":
-division(1, ZERO)
+if __name__ == "__main__":
+    division(1, ZERO)
