76f9957a8e4ac1b1174904260d35ced5af6c7329
Pavel <pavelmpd82@gmail.com>
2016-09-13 11:18:16 +0000

    Add first test

--- dj_post/postsite/polls/tests.py
+++ dj_post/postsite/polls/tests.py
@@ -1,3 +1,12 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        question = Question(question_text="some text", pub_date=(timezone.now() + datetime.timedelta(days=15)).date())
+        self.assertFalse(question.was_published_recently())
