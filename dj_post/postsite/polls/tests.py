ee46d18d3cb4ac63ce993cb42235bedc360c88f2
Pavel <pavelmpd82@gmail.com>
2016-09-13 11:27:07 +0000

    Add some unit tests

--- dj_post/postsite/polls/tests.py
+++ dj_post/postsite/polls/tests.py
@@ -10,3 +10,11 @@ class QuestionTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         question = Question(question_text="some text", pub_date=(timezone.now() + datetime.timedelta(days=15)).date())
         self.assertFalse(question.was_published_recently())
+
+    def test_was_published_recently_with_ols_question(self):
+        question = Question(question_text="some text", pub_date=(timezone.now() - datetime.timedelta(days=2)).date())
+        self.assertFalse(question.was_published_recently())
+
+    def test_was_published_recently_with_recently_question(self):
+        question = Question(question_text="some text", pub_date=(timezone.now()).date())
+        self.assertTrue(question.was_published_recently())
